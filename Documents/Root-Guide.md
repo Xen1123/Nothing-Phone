18b9f61e54e85eec3d3fb44c42742dbf71dff842
Xen1123 <eluce11232011@gmail.com>
2026-05-09 23:12:55 -0400

    Made Nothing.py as replacement for nothing flasher and nothing restore while being Windows, MacOS, and Linux compatible

--- Documents/Root-Guide.md
+++ Documents/Root-Guide.md
@@ -85,7 +85,7 @@ fastboot flash init_boot root_init_boot.img && fastboot reboot
   - [Droidify](https://github.com/Droid-ify/client/releases/download/v0.7.1/app-release.apk) Is An App Store Of A Collection Of Free And Open Source (FOSS) Apps That You Can Download, Some Apps Available In Droidify Are Directly Reliant On LSPosed/Vector
   - Play Integrity
     - When you unlock an Android bootloader, root the device, or flash a custom ROM, your device is considered "bad" to Google's servers. Apps that rely on Play Integrity, this is device dependent, Google Pixels are typically very good at Play Integrity, even with unlocked, modded devices. Although Pixels are typically safe, other phones tend to have broken integrity just from unlocking the bootloader - Magisk modules do exist to help you, though, but on select devices, you'll need to flash all of these.
-      - [Strong Play Integrity Fix](https://github.com/sarhan00/Strong-Integrity-Fix/releases/download/v10.0.21/Play.Integrity.Fix.-.Strong.Integrity.-v10.0.21.zip)
+      - [Strong Play Integrity Fix](https://github.com/sarhan00/Strong-Integrity-Fix/releases/download/v10.0.22/Play.Integrity.Fix.-.Strong.Integrity.-v10.0.22.zip)
       - [Rezygisk (Use Instead of Magisk Zygisk)](https://github.com/PerformanC/ReZygisk/releases/download/v1.0.0-rc.7/ReZygisk-v1.0.0-rc.7-release.zip)
       - [Tricky Store](https://github.com/5ec1cff/TrickyStore/releases/download/1.4.1/Tricky-Store-v1.4.1-245-72b2e84-release.zip)
       - [YuriKey Manager](https://github.com/Yurii0307/yurikey/releases/download/v3.0.4/Yurikey-v3.0.4.signed.zip)
---
+++ Nothing.py
@@ -0,0 +1,128 @@
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+import os
+import time
+print(r"""
+__   _  ___ _____ _   _ ___ _   _  ____ 
+| \ | |/ _ \_   _| | | |_ _| \ | |/ ___|
+|  \| | | | || | | |_| || ||  \| | |  _ 
+| |\  | |_| || | |  _  || || |\  | |_| |
+|_| \_|\___/ |_| |_| |_|___|_| \_|\____|
+                                        
+""")
+print("This Script Restores Your Nothing Phone Firmware Or Flashes A Custom ROM Based On What Images Are In Your Directory!")
+confirm = input("Are Your Images In Your Current Directory? (y/n)")
+if confirm.lower() != "y":
+     print("Please Make Sure Your Images Are Right! <3")
+     sys.exit()
+else:
+     confirm = input("Are You REALLY Sure? On Some Devices, Restoring From EDL Is A Real Pain... (y/n)")
+     if confirm.lower() != "y":
+          print("Second Thoughts I See...")
+          sys.exit()
+     else:
+          print("Alright Then!")
+
+adb_path = shutil.which("adb")
+if not adb_path:
+     print("ADB Not Installed Or In Your PATH!")
+     sys.exit()
+else:
+     print(f"ADB Found At: {adb_path}")
+
+fastboot_path = shutil.which("fastboot")
+if not fastboot_path:
+     print("Fastboot Not Installed Or In Your PATH!")
+     sys.exit()
+else:
+     print(f"Fastboot Found At: {fastboot_path}")
+
+print("Rebooting To Bootloader Fastboot (ABL)")
+subprocess.run([
+     "adb", "reboot", "bootloader"
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+subprocess.run([
+     "fastboot", "reboot", "bootloader"
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+if os.name == 'nt':
+     os.system('cls')
+else:
+     os.system('clear')
+
+images = [
+     "boot", "vendor_boot", "dtbo", "recovery", "abl", "aop", "aop_config", "bluetooth", "cpucp_dtb", "cpucp", 
+     "devcfg", "dsp", "featenabler", "hyp", "imagefv", "init_boot", "keymaster", "modem", "multiimgoem", "odm", 
+     "pvmfw", "qupfw", "shrm", "tz", "uefi", "uefisecapp", "vbmeta", "vbmeta_system", "vbmeta_vendor", "xbl", 
+     "xbl_config", "xbl_ramdump"
+]
+
+for img in images:
+     file_path = Path(f"{img}.img")
+
+     if file_path.is_file():
+          print(f"\nFlashing {img} . . .")
+          subprocess.run([
+               "fastboot", "flash", img, file_path
+          ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+if os.name == 'nt':
+     os.system('cls')
+else:
+     os.system('clear')
+
+print(r"""
+__   _  ___ _____ _   _ ___ _   _  ____ 
+| \ | |/ _ \_   _| | | |_ _| \ | |/ ___|
+|  \| | | | || | | |_| || ||  \| | |  _ 
+| |\  | |_| || | |  _  || || |\  | |_| |
+|_| \_|\___/ |_| |_| |_|___|_| \_|\____|
+                                        
+          Going To Fastbootd
+""")
+
+subprocess.run([
+     "fastboot", "reboot", "fastboot"
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+time.sleep(5)
+
+images = [
+     "product", "system", "system_dlkm", "system_ext", "vendor", "vendor_dlkm"
+]
+
+for img in images:
+     file_path = Path(f"{img}.img")
+
+     if file_path.is_file():
+          print(f"Flashing {img}")
+          subprocess.run([
+               "fastboot", "flash", img, file_path
+          ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+confirm = input("Reboot To System? (y/n)")
+if confirm.lower() != "y":
+     print("Okay! Staying Put!")
+     sys.exit()
+else:
+     subprocess.run([
+          "fastboot", "reboot"
+     ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+if os.name == 'nt':
+     os.system('cls')
+else:
+     os.system('clear')
+
+print(r"""
+__   _  ___ _____ _   _ ___ _   _  ____ 
+| \ | |/ _ \_   _| | | |_ _| \ | |/ ___|
+|  \| | | | || | | |_| || ||  \| | |  _ 
+| |\  | |_| || | |  _  || || |\  | |_| |
+|_| \_|\___/ |_| |_| |_|___|_| \_|\____|
+                                        
+                All Done!
+""")
